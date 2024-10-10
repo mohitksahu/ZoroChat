15f260997464b1c558622e7f929db968c3b3459b
Mohit Kumar Sahu <mohitkumarsahu0110@gmail.com>
2024-10-10 15:30:50 +0530

    Done with the backend work

--- js/client.js
+++ js/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000')
+const socket = io('http://localhost:8000')
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp')
@@ -12,5 +12,38 @@ socket.on('connect_error', (err) => {
     console.log('Connection error:', err);
 });
 
-const names = prompt("Enter tour name to join");
-socket.emit('new-user-joined', names) 
+const append = (message, position) => {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+};
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = '';
+});
+
+const name = prompt("Enter your name to join");
+console.log(name);
+socket.emit('new-user-joined', name);
+
+socket.on('user-joined', name => {
+    append(`${name} joined the chat`, 'left');
+})
+
+socket.on('current-users', (currentUsers) => {
+    append(`People in the Chat: ${currentUsers.join(', ')}`, 'left');
+});
+
+socket.on('receive', data => {
+    append(`${data.name}: ${data.message}`, 'left');
+})
+
+socket.on('left', name => {
+    append(`${name} left the chat`, 'left');
+})
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,17 +1,29 @@
-//node server which will handle socket io connections
-
-const io = require('socket.io')(3000)
+const io = require('socket.io')(8000, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
 
 const users = {};
 
-io.on('connection', socket =>{
-    socket.on('new-user-joined', name =>{
-        console.log("New user", name);
-        users[socket.id] = name;
-        socket.broadcast.emit('user-joined', name);
-    });
+io.on('connection', socket => {
+  socket.on('new-user-joined', (name) => {
+    //console.log("New user", name)
+    users[socket.id] = name;
+    socket.broadcast.emit('user-joined', name);
 
-    socket.on('send', message =>{
-        socket.broadcast.emit('receive', {message: message, name: users[socket.id]});
-    })
-})
+    socket.emit('current-users', Object.values(users));
+  });
+
+  socket.on('send', (message) => {
+    socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
+  });
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('left', users[socket.id]);
+    delete users[socket.id];
+  });
+});
